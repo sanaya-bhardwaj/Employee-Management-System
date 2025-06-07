c0cc89cc495068449e286c3330f33459df596007
Sanaya Bhardwaj <135012941+sanaya-bhardwaj@users.noreply.github.com>
2025-06-07 23:39:19 +0530

    Add files via upload

---
+++ Employee Management System/src/employee/management/system/Conn.java
@@ -0,0 +1,15 @@
+package employee.management.system;
+import java.sql.*;
+public class Conn {
+    Connection c;
+    Statement s;
+    public Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c=DriverManager.getConnection("jdbc:mysql://localhost:3306/employeemanagementsystem", "root", "12345");
+            s=c.createStatement();
+        }catch( Exception e){
+            e.printStackTrace();
+        }
+    }
+}
